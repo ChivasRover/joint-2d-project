600e70407c98c8f8fcde89a60c97b979b92729d8
ChivasRover <chivas.unity@gmail.com>
2020-01-07 20:54:49 +0300

    Update NewBehaviourScript.cs

--- joint-2d-project/Assets/NewBehaviourScript.cs
+++ joint-2d-project/Assets/NewBehaviourScript.cs
@@ -7,12 +7,12 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hell to world");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("hell to world");
+
     }
 }
