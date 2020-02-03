78b0aeb1d4c310454b1257d320eacf1fc3a7c73b
Sinen <59580037+Sinenn@users.noreply.github.com>
2020-02-03 21:47:32 +0500

    Added attack animation time out and fixed animation controller

--- joint-2d-project/Assets/Scripts/Player scripts/Combat/Weapon.cs
+++ joint-2d-project/Assets/Scripts/Player scripts/Combat/Weapon.cs
@@ -8,17 +8,23 @@ public class Weapon : MonoBehaviour
     public Animator animator;
     public Transform firePoint;
     public GameObject bulletPrefab;
+    public float attackTimeOut = 0;
+    public float attackAnimationTimeOut = 0.5f;
 
     void Update()
     {
         if (Input.GetButtonDown("Fire"))
         {
-            Shoot();
+            Invoke("Shoot", attackTimeOut);
         }
     }
     void Shoot()
     {
         animator.SetBool("isAttacking", true);
+        Invoke("spawnBullet", attackAnimationTimeOut);
+    }
+    private void spawnBullet()
+    {
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         animator.SetBool("isAttacking", false);
     }
