15bf816dee6fea49882063ab75f3e32ead66e1f5
Sinen <59580037+Sinenn@users.noreply.github.com>
2020-02-11 11:52:42 +0500

    Created retro health counter
    Later we can change it to health bar

---
+++ joint-2d-project/Assets/Scripts/Player scripts/other/HealthDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    public Text health_counter;
+    public PlayerStatsControll stats;
+    void Start()
+    {
+        
+    }
+    void LateUpdate()
+    {
+        health_counter.text = "HP: " + stats.playerHealth;
+    }
+}
