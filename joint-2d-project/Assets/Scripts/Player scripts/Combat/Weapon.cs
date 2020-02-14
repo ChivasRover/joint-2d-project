1a65354069e3fb97aa0ce8da8eea7faf1edee7bc
Sinen <59580037+Sinenn@users.noreply.github.com>
2020-02-15 01:16:46 +0500

    Fixed attack(again)

--- joint-2d-project/Assets/Scripts/Player scripts/Combat/Weapon.cs
+++ joint-2d-project/Assets/Scripts/Player scripts/Combat/Weapon.cs
@@ -9,32 +9,26 @@ public class Weapon : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPrefab;
     public bool isReadyToAttack = true;
-    public float attackDelay = 1f;
-    public float attackAnimationTimeOut = 0.5f;
 
     void Update()
-    {
-        
+    {        
         if (Input.GetButtonDown("Fire"))
         {
             if(isReadyToAttack)
             {
-                Shoot();
-                isReadyToAttack = false;
-                Invoke("getReadyToAttack", attackDelay);
+                animator.SetBool("isAttacking", true);
             }
         }
     }
-    void Shoot()
-    {
-        animator.SetBool("isAttacking", true);
-        Invoke("spawnBullet", attackAnimationTimeOut);
-    }
-    private void spawnBullet()
+    public void Shoot()
     {
         FindObjectOfType<AudioManager>().Play("ShootSound");
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+    
+    public void endOfAttack()
+    {
+        isReadyToAttack = true;
         animator.SetBool("isAttacking", false);
     }
-    private void getReadyToAttack() => isReadyToAttack = true;
 }
